11083283e3c24299249809a6ab88d6f112d82a06
jeromesnail <jeromesnail@hotmail.fr>
2018-03-05 12:53:22 +0100

    final project

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const url = require('url');
+const path = require('path');
+const app = express();
+const port = /*process.env.PORT*/ '1337';
+
+const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const defaultResult = { 'unix': null, 'natural': null };
+let result = {};
+let date;
+
+app.get('/:time', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  const time = req.params.time;
+  if (isNaN(time)) date = new Date(time);
+  else date = new Date(time * 1000);
+  if (isNaN(date.getTime())) result = defaultResult;
+  else result = {
+    'unix': date.getTime() / 1000,
+    'normal': month[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
+  }
+  res.end(JSON.stringify(result));
+});
+
+app.get('*', (req, res) => {
+  result = defaultResult;
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(result));
+});
+
+app.listen(port);
